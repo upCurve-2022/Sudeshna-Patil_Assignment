40991d80fc2d7f6997e86aa09724230fea7e256c
SUDESHNA1234 <69962929+SUDESHNA1234@users.noreply.github.com>
2022-03-31 14:53:22 +0530

    Add files via upload

---
+++ Arithmetic_Expressions.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Arithmetic_Expressions {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int expression=5+5+(2*4)-(2/2);
+
+        System.out.println(expression);
+    }
+}
