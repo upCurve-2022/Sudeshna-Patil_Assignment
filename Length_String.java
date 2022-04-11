644d70ed29a024a1eaa8a92ad909d2517eee810e
SUDESHNA1234 <69962929+SUDESHNA1234@users.noreply.github.com>
2022-04-12 00:23:17 +0530

    Add files via upload

---
+++ Length_String.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Length_String {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String str=new String();
+        System.out.println("Enter the String :");
+        str=sc.nextLine();
+        System.out.println("Length of the "+str+" is "+str.length());
+    }
+}
